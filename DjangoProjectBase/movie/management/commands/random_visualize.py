cfd852541e036f52a01d0e0a8547fdf03651f898
Emanuel Gonzalez <egonzalezq@eafit.edu.co>
2025-04-02 19:02:38 -0500

    adding embeddings to recommend movies

---
+++ DjangoProjectBase/movie/management/commands/random_visualize.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+import requests
+from django.core.management.base import BaseCommand
+from movie.models import Movie
+
+class Command(BaseCommand):
+    help = "Visualize random movie embeddings"
+
+    def handle(self, *args, **kwargs):
+
+        movies = list(Movie.objects.all())
+        
+        if not movies:
+            self.stdout.write(self.style.ERROR("No hay películas en la base de datos"))
+            return
+
+        movie = random.choice(movies)
+        embedding_vector = np.frombuffer(movie.emb, dtype=np.float32)
+
+        self.stdout.write(self.style.SUCCESS(f"🎬 Película seleccionada: {movie.title}"))
+        self.stdout.write(f"📊 Embeddings (primeros 5 valores): {embedding_vector[:5]}")
